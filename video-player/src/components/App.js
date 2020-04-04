85ff7f1a39cac714e4edb0cbdf35f6bc044a33c1
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-04 15:28:10 +0100

    Create onVideoSelect callback and pass it down as a prop from our App component down through VideoList to VideoItem

--- video-player/src/components/App.js
+++ video-player/src/components/App.js
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
   
   onTermSubmit = async term => {
     const KEY = 'AIzaSyAPVtFX1-MfrL_LONvpYCDa3chTzDuRQBc';
@@ -22,11 +22,15 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video })
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={ this.state.videos }/>
+        <VideoList onVideoSelect={this.onVideoSelect} videos={ this.state.videos }/>
       </div>
     );
   }
