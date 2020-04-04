0ff4d0174e7a9d1ba5a05e9e142195e8e246e001
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-04 13:18:53 +0100

    Create onFormSubmit function to send API request

--- video-player/src/components/App.js
+++ video-player/src/components/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
+  state = { videos: [] };
+  
+  onTermSubmit = async term => {
+    const KEY = 'AIzaSyAPVtFX1-MfrL_LONvpYCDa3chTzDuRQBc';
+
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: 'video',
+        key: `${KEY}`
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+  };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos.
       </div>
     );
   }
