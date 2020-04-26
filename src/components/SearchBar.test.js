c8bdf233b91ab2c0fc83db4af60cb9a8e58d6241
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-26 14:31:42 +0100

    test: SearchBar onFormSubmit and onInputChange events

--- src/components/SearchBar.test.js
+++ src/components/SearchBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
-import { findByTestAttr, checkProps } from '../test/testUtils';
+import { shallow, mount } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
 
 import SearchBar from './SearchBar';
 
@@ -10,8 +10,30 @@ const setup = (props={}, state=null) => {
     return wrapper;
 }
 
-it("renders the VideoDetail component", () =>{
-  const wrapper = setup({}, { term: 'cat' });
+it("renders the SearchBar component", () =>{
+  const wrapper = setup({}, { term: '' });
   const searchBarComponent = findByTestAttr(wrapper, 'component-search-bar');
   expect(searchBarComponent.length).toBe(1);
+});
+
+it('should call onFormSubmit when a search term is submitted', () => {
+  const mockFunction = jest.fn();
+
+  const component = mount(
+    <SearchBar onFormSubmit={mockFunction} />
+  );
+  component.find('form').simulate('submit');
+  expect(mockFunction).toHaveBeenCalled();
+  
+  component.unmount();
+});
+
+it("should change the 'term' state when the input is changed", () => {
+  const component = mount(<SearchBar />);
+
+  expect(component.state()).toEqual({ term: "" })
+  component.find('input').simulate('change', { target: { name: 'term', value: "test" }});
+  expect(component.state()).toEqual({ term: "test" })
+
+  component.unmount();
 });
