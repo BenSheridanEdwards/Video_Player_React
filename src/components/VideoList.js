6d96a35e6cbc2541f7242fdba144999f66fd55d7
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-26 14:33:03 +0100

    refactor: remove console.log from line 5

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
-  console.log(videos)
   const renderedList = videos.map((video) => {
     return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} data-test="component-video-item"/>;
   });
