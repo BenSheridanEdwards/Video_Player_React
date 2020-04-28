bb0523767fd01663ccd8a825e365293e0c8e5cb1
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-28 13:16:17 +0100

    test: youtube api call in onTermSubmit function

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow, ShallowWrapper, mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
+import mockAxios from 'axios'
 
 import App from './App';
 
@@ -40,4 +41,19 @@ it('calls componentDidMount', () => {
   jest.spyOn(App.prototype, 'componentDidMount')
   const wrapper = shallow(<App />)
   expect(App.prototype.componentDidMount.mock.calls.length).toBe(1)
+})
+
+it('calls axios and returns youtube videos', async () => {
+  mockAxios.get.mockImplementationOnce(() => 
+    Promise.resolve({
+      data: { 
+        items: [] 
+      }
+    }))
+ 
+  const wrapper = setup()
+  const videos = await wrapper.instance().onTermSubmit('cats')
+  expect(wrapper.state()).toEqual({"selectedVideo": undefined, "videos": []})
+  expect(mockAxios.get).toHaveBeenCalledTimes(6);
+  expect(mockAxios.get).toHaveBeenCalledWith("https://www.googleapis.com/youtube/v3/search", {"params": {"key": "AIzaSyAPVtFX1-MfrL_LONvpYCDa3chTzDuRQBc", "maxResults": 5, "part": "snippet", "q": "Cat", "type": "video"}})
 })
